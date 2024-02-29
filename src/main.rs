3048cf8bd7a70d554692117e993d2ca5f8cb290a
i1vy <evennodegard@gmail.com>
2024-02-29 23:18:15 +0100

    2 unwraps on the same line?

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 use std::path::Path;
-use std::env;
+use std::{env, fs};
 
 fn main() {
 	let localappdata = env::var("LOCALAPPDATA").expect("couldn't find LOCALAPPDATA env");
 	let versions = Path::new(&localappdata).join("Roblox\\Versions");
 
+	// exit if roblox is not in localappdata
 	if !versions.exists() {
 		println!("roblox is probably not installed");
 		std::process::exit(1);
 	}
 
-	
+	// the versions directory
+	let versions = fs::read_dir(versions).expect("no versions found");
+
+	// for every version found
+	for version in versions {
+		let versionname = version.as_ref().unwrap().file_name().into_string().unwrap();
+
+		// if it is actually a version thing
+		if versionname.starts_with("version") {
+			println!("{:?}", version.unwrap().file_name())
+		}
+	}
 	
 }
